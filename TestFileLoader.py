022dc894e83ef2550752b40a0307b50aaffaf154
Giuseppe Rizzo <g.rizzo87@gmail.com>
2020-06-06 13:56:34 +0200

    added stratified and repeated cv

--- TestFileLoader.py
+++ TestFileLoader.py
@@ -29,28 +29,30 @@ n_inputs = len(dataset.getInstances()[0])
 #print("Input", n_inputs, dataset.getInstances()[60])
 n_outputs =  2 #len(set([row[-1] for row in dataset.getLabels()]))
 
-X_train, X_test, Y_train, Y_test = dataset.split_training_test(0.2)
-dataset.llov()
+#X_train, X_test, Y_train, Y_test = dataset.split_training_test(0.2)
+#print(X_train, X_test)
+print("Cv",dataset.cross_validation(3))
+print("llov",dataset.llov())
 builder = Builder()
 # create a multilayer perceptron for classification
-network_creator = AbstractNetworkCreator().createNetworkCreator()
-layers = [LayerFactory.getInputLayer(n_inputs), LayerFactory.getDenseLayer(n_inputs, ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.relu))), LayerFactory.getDenseLayer(n_outputs,ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.softmax)))]
-optimizer = Optimizers.lookup(OptimizersName.fromValue(OptimizersName.adadelta))
-loss_function_lookup = LossFunction.lookup(LossFunctionsName.fromValue(LossFunctionsName.sparse_categorical_crossentropy))
-mln = network_creator.createNetwork(builder, layers, layers[0], layers[-1],optimizer, loss_function_lookup,['accuracy'])
-mln.fit(dataset.getInstances(),label,200)
-print(label, dataset.getLabels())
-evaluation = mln.evaluate(dataset.getInstances(),dataset.getLabels())
-print ("Loss: ",evaluation[0])
-print ("Accuracy: ",evaluation[1])
-
-#create a logistic regression
-layers = [LayerFactory.getInputLayer(n_inputs),LayerFactory.getDenseLayer(1, ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.softmax)))]
-builder2 = Builder()
-optimizer2 = Optimizers.lookup(OptimizersName.fromValue(OptimizersName.adam))
-loss = LossFunction.lookup(LossFunctionsName.fromValue(LossFunctionsName.MAE))
-logistic_regressor = AbstractNetworkCreator.createNetworkCreator().createNetwork(builder2,layers,layers[0],layers[-1],optimizer2,loss,['accuracy'])
-logistic_regressor.fit(dataset.getInstances(),dataset.getLabels(),200)
+# network_creator = AbstractNetworkCreator().createNetworkCreator()
+# layers = [LayerFactory.getInputLayer(n_inputs), LayerFactory.getDenseLayer(n_inputs, ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.relu))), LayerFactory.getDenseLayer(n_outputs,ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.softmax)))]
+# optimizer = Optimizers.lookup(OptimizersName.fromValue(OptimizersName.adadelta))
+# loss_function_lookup = LossFunction.lookup(LossFunctionsName.fromValue(LossFunctionsName.sparse_categorical_crossentropy))
+# mln = network_creator.createNetwork(builder, layers, layers[0], layers[-1],optimizer, loss_function_lookup,['accuracy'])
+# mln.fit(dataset.getInstances(),label,200)
+# print(label, dataset.getLabels())
+# evaluation = mln.evaluate(dataset.getInstances(),dataset.getLabels())
+# print ("Loss: ",evaluation[0])
+# print ("Accuracy: ",evaluation[1])
+#
+# #create a logistic regression
+# layers = [LayerFactory.getInputLayer(n_inputs),LayerFactory.getDenseLayer(1, ActivationFunction.lookup(ActivationFunctionsName.fromValue(ActivationFunctionsName.softmax)))]
+# builder2 = Builder()
+# optimizer2 = Optimizers.lookup(OptimizersName.fromValue(OptimizersName.adam))
+# loss = LossFunction.lookup(LossFunctionsName.fromValue(LossFunctionsName.MAE))
+# logistic_regressor = AbstractNetworkCreator.createNetworkCreator().createNetwork(builder2,layers,layers[0],layers[-1],optimizer2,loss,['accuracy'])
+# logistic_regressor.fit(dataset.getInstances(),dataset.getLabels(),200)
 
 
 
--- datasource/DB.py
+++ datasource/DB.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from datasource.FileLoader import FileLoader
-from sklearn.model_selection import train_test_split, KFold,LeaveOneOut
+from sklearn.model_selection import train_test_split, KFold,LeaveOneOut, RepeatedKFold, StratifiedKFold, RepeatedStratifiedKFold
 class Dataset:
   def __init__(self,file):
     a = FileLoader('csv',file)
@@ -22,10 +22,34 @@ class Dataset:
   def split_training_test(self, test_size_rate):
     return train_test_split(self.__instances__,self.__labels__,test_size=test_size_rate)
 
-  def cross_validation(self, nOfFold):
-     splitter =KFold(n_splits=nOfFold)
-     return splitter.split(self.__instances__,self.__labels__)
+  def cross_validation(self, folds):
+      splitter = KFold(n_splits=folds)
+      return self.split(self.__array__,splitter)
+
+
+  def split(self, dataset, splitter):
+    splits = []
+    for tr, te, in splitter.split(dataset):
+      print("XXXX", dataset[tr])
+      X_train = dataset[tr, :-1]
+      Y_train = dataset[tr, -1]
+      X_test = dataset[te, :-1]
+      Y_test = dataset[te, -1]
+      splits.append({"x_training": X_train, "y_training": Y_train, "x_test": X_test, "y_training": Y_test})
+    return splits
 
   def llov(self):
     splitter = LeaveOneOut()
-    return splitter.split(self.__instances__,self.__labels__)
+    return self.split(self.__array__,splitter)
+
+  def repeatedCV(self, folds, replicates):
+    splitter= RepeatedKFold(n_splits=folds,n_repeats=replicates)
+    return self.split(self.__array__,splitter)
+
+  def stratifiedCV(self, folds):
+    splitter = StratifiedKFold(folds)
+    return self.split(self.__array__,splitter)
+
+  def repeatedStratifiedCV(self, folds, replicates):
+    splitter = RepeatedStratifiedKFold(n_splits=folds,n_repeats=replicates)
+    return self.split(self.__array__,splitter)
